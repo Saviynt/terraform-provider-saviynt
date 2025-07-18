66266095680a778f6d9a4dafa677aa27c4fb87c4
Nandini A Choukimath <nandini.choukimath@saviynt.com>
2025-07-18 05:47:08 +0000

    TER-139: Added dynamic attribute datasource in Terraform and modified copywrite.hcl to generate license automatically

--- examples/data-sources/saviynt_rest_connection_datasource/data-source.tf
+++ examples/data-sources/saviynt_rest_connection_datasource/data-source.tf
@@ -1,4 +1,4 @@
-// Copyright (c) Saviynt Inc.
+// Copyright (c) 2025 Saviynt Inc.
 // SPDX-License-Identifier: MPL-2.0
 
 data "saviynt_rest_connection_datasource" "rest" {
--- examples/resources/saviynt_sap_connection_resource/resource.tf
+++ examples/resources/saviynt_sap_connection_resource/resource.tf
@@ -1,4 +1,4 @@
-// Copyright (c) Saviynt Inc.
+// Copyright (c) 2025 Saviynt Inc.
 // SPDX-License-Identifier: MPL-2.0
 
 resource "saviynt_sap_connection_resource" "ss" {
